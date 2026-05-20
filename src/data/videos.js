c99361269f024346bcb478e01a0d8aed2230b173
Alok Kumar <aloknh31@gmail.com>
2026-05-20 22:10:03 +0530

    added videos id all

--- src/data/videos.js
+++ src/data/videos.js
@@ -28,5 +28,50 @@ export const videos = [
     id: "_kx92sDfJ2g",
     title: "Part-4",
     category: "Alok-Side"
+  },
+  {
+    id: "GQYYDHv175o",
+    title: "Song-1",
+    category: "Alok-Side"
+  },
+  {
+    id: "CbTxqp-z2Qs",
+    title: "Song-2",
+    category: "Alok-Side"
+  },
+  {
+    id: "-WqFA2qfvWU",
+    title: "Song-3",
+    category: "Alok-Side"
+  },
+  {
+    id: "3RFPY9yrc1M",
+    title: "Song-4",
+    category: "Alok-Side"
+  },
+  {
+    id: "SFha7F6Xcrg",
+    title: "Sneha & Alok closeup",
+    category: "Sneha-Side"
+  },
+  {
+    id: "cxLud5PmaHA",
+    title: "Sneha & Alok mix 1",
+    category: "Sneha-Side"
+  },
+  {
+    id: "fUsDLN-w8Zo",
+    title: "Sneha & Alok mix 2",
+    category: "Sneha-Side"
+  },
+  {
+    id: "Dsymwepw3U4",
+    title: "Sneha & Alok mix 3",
+    category: "Sneha-Side"
+  },
+  {
+    id: "zh_ZGgMtXeo",
+    title: "SNEHA & ALOK Wedding Teaser",
+    category: "Sneha-Side"
   }
 ];
